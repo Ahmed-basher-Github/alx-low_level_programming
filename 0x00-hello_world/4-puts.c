01ee56cb49fd0e924015d4d66a2153b179c593fe
Ahmed Basher Abdalla <basherahmed362@gmail.com>
2023-09-07 06:00:24 -0700

    u

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
-/* Entey point of the programm and return 0 */
+/**
+ * main - Program entry point
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
