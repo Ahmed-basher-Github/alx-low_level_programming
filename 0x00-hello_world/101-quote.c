051fa7959b58535a4d50038f25f790b81b2a4abf
Ahmed Basher Abdalla <basherahmed362@gmail.com>
2023-09-07 06:45:44 -0700

    u

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,7 +8,7 @@
 int main(void)
 {
 	write(2,
-	"and that piece of art is userfil\" - Dora Korpar, 2015-10-19\n", 59);
+	"and that piece of art is userful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 
 }
