cca4eaa9be0d5fb202e374d403bce0632c0c504e
Ahmed Basher Abdalla <basherahmed362@gmail.com>
2023-09-08 07:29:33 -0700

    u

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - entry point of the program
+ *
+ * Return: 0 Always (success)
+ */
+int main(void)
+{
+	int i, x;
+
+	for (i = 0; i < 90; i++)
+	{
+		putchar('0' + (i / 10));
+		putchar('0' + (i % 10));
+		if (i != 89)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
