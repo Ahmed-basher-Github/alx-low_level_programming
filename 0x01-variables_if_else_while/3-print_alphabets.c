b74561233795d330f1f18eb1b55a10898155b85d
Ahmed Basher Abdalla <basherahmed362@gmail.com>
2023-09-08 06:01:09 -0700

    u

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * main - program Entry point
+ *
+ * Return: 0 Always
+ */
+int main(void)
+{
+	const char *abces = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	int i;
+
+	for (i = 0; abces[i] != '\0'; i++)
+	{
+		putchar(abces[i]);
+	}
+
+	putchar('\n');
+	return (0);
+}
