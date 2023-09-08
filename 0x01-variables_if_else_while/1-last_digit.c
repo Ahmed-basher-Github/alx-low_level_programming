811027de34e4f9daaef379b3f9aff9c1ea17cfce
Ahmed Basher Abdalla <basherahmed362@gmail.com>
2023-09-08 05:34:46 -0700

    u

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -23,7 +23,7 @@ int main(void)
 	}
 	else if (x == 0)
 	{
-		printf("Last digit of %d is %d amd is 0\n", n, x);
+		printf("Last digit of %d is %d and is 0\n", n, x);
 	}
 	else if (x < 6 && x != 0)
 	{
