2caafa2a9162738a3bd891697a1a927a69c840b9
Ahmed Basher Abdalla <basherahmed362@gmail.com>
2023-09-08 06:20:51 -0700

    u

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - program entry point
+ *
+ * Return: 0 Always (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d", i);
+	}
+	putchar('\n');
+
+	return (0);
+}
+
