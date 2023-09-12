aa91c380e1ca0bb04a5dc6896b3e6370a5c51309
Ahmed Basher Abdalla <basherahmed362@gmail.com>
2023-09-12 06:25:41 -0700

    u

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * add - a functoin that adds two numbers
+ * @x: the first number entered by user
+ * @y: the second number entered by user
+ * Return: type int the sum of the two numbers
+ */
+int add(int x, int y)
+{
+	return (x + y);
+}
