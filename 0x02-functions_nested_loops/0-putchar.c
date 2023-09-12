157b8628b2572b1a058efa33e8c62075c45ba91f
Ahmed Basher Abdalla <basherahmed362@gmail.com>
2023-09-11 20:55:18 -0700

    u

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Return: Always return 0 (Success)
+ */
+int main(void)
+{
+	const char message[] = "_putchar";
+
+	printf("%s\n", message);
+	return (0);
+}
