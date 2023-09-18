b3078291ca18e0165314b374b444d75cd90087ee
Ahmed Basher Abdalla <basherahmed362@gmail.com>
2023-09-18 09:08:28 -0700

    u

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+/**
+ * puts_half - divids a string into two halfs
+ * @str: the string
+ * Return:nothing function of type void
+ */
+
+void puts_half(char *str)
+{
+	int len;
+
+	len = strlen(str);
+
+	if (len > 1)
+	{
+		int split = len / 2;
+
+		printf("%s\n", str + split);
+	}
+
+
+}
