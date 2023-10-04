5eec9f9dfaa311b809bca17d337583a1ccb456f6
Ahmed Basher Abdalla <basherahmed362@gmail.com>
2023-10-04 04:38:49 -0700

    u

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,18 +1,17 @@
-#include <stdio.h>
 #include "main.h"
+#include <stdio.h>
 #include <stdlib.h>
 
-
-int world_len(char *str);
+int word_len(char *str);
 int count_words(char *str);
 char **strtow(char *str);
 
 /**
- * word_len - locates the index
- * marking the end of the first
- * word containg within a string
- * @str: the string
- * Return:ending of inital word
+ * word_len - Locates the index marking the end of the
+ * first word contained within a string.
+ * @str: the string to be searched.
+ *
+ * Return: the index marking the end of the initail word.
  */
 int word_len(char *str)
 {
@@ -25,7 +24,62 @@ int word_len(char *str)
 	}
 	return (len);
 }
+/**
+ * count_words - counts the number of words contained within a string
+ * @str: the string to be searched
+ *
+ * Return: the index marking the end of the initail word pointed to by
+ * str
+ */
+int count_words(char *str)
+{
+	int index = 0, words = 0, len = 0;
 
+	for (index = 0; *(str + index); index++)
+		len++;
+	for (index = 0; index < len; index++)
+	{
+		if (*(str + index) != ' ')
+		{
+			words++;
+			index += word_len(str + index);
+		}
+	}
+	return (words);
+}
+/**
+ * strtow - splits a string into words.
+ * @str: the string to be split.
+ *
+ * Return: if str = NULL, str = "", or function fails - NULL
+ * otherwise - a pointer to an array of strings (words).
+ */
+char **strtow(char *str)
+{
+	char **strings;
+	int index = 0, words, w, letters, l;
+
+	if (str == NULL || str[0] == '\0')
+		return (NULL);
+
+	words = count_words(str);
+	if (words == 0)
+		return (NULL);
+
+	strings = malloc(sizeof(char *) * (words + 1));
+	if (strings == NULL)
+		return (NULL);
+
+	for (w = 0; w < words; w++)
+	{
+		while (str[index] == ' ')
+			index++;
+		letters = word_len(str + index);
+
+		strings[w] = malloc(sizeof(char) * (letters + 1));
+
+		if (strings[w] == NULL)
+		{
 			for (; w >= 0; w--)
 				free(strings[w]);
 			free(strings);
@@ -34,14 +88,9 @@ int word_len(char *str)
 
 		for (l = 0; l < letters; l++)
 			strings[w][l] = str[index++];
-
 		strings[w][l] = '\0';
 	}
 	strings[w] = NULL;
-
 	return (strings);
 }
-
-
-
 
