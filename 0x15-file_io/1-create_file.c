47815274b16ef9ecab1757f7720a34d505f119be
Ahmed Basher Abdalla <basherahmed362@gmail.com>
2023-10-30 03:17:28 -0700

    u

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -22,7 +22,8 @@ int create_file(const char *filename, char *text_content)
 	if (text_content == NULL)
 		text_content = "";
 
-	for (nletters = 0; text_content[nletters]; nletters++);
+	for (nletters = 0; text_content[nletters]; nletters++)
+		;
 
 	rwr = write(fd, text_content, nletters);
 
@@ -31,5 +32,4 @@ int create_file(const char *filename, char *text_content)
 	close(fd);
 
 	return (1);
-
 }
